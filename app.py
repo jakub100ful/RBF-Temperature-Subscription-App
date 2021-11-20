727d04f4f3fc3633a396f82319aff83e77dd134e
jakub100ful <56305120+jakub100ful@users.noreply.github.com>
2021-11-20 21:11:12 +0000

    Added D3 viz

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+
+from rbf import getTempData
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    dataList = getTempData()
+
+    print(dataList)
+
+    return render_template('index.html', data=dataList)
--- rbf.py
+++ rbf.py
@@ -1,41 +1,59 @@
 from scipy.interpolate import Rbf
 import numpy as np
 from pprint import pprint
+import csv
 
-with open("sensor_data.txt") as textFile:
-    lines = [line.strip('\n').split(',') for line in textFile]
 
-x = []
-y = []
-z = []
-d = []
-n = len(lines)
+def getTempData():
 
-i = 0
+    with open("sensor_data.txt") as textFile:
+        lines = [line.strip('\n').split(',') for line in textFile]
 
-while i < n:
-    x.append(lines[i][0])
-    y.append(lines[i][1])
-    z.append(lines[i][2])
-    d.append(lines[i][3])
+    x = []
+    y = []
+    z = []
+    d = []
 
-    i += 1
+    n = len(lines)
+    a = (n*2)-1
 
-rbfi = Rbf(x, y, z, d)  # radial basis function interpolator instance
-# coordinates to be interpolated / predicted
-xi = input("X coordinate: ")
-yi = input("Y coordinate: ")
-zi = input("Z coordinate: ")
+    i = 0
 
-di = rbfi(xi, yi, zi)   # interpolated values
+    while i < n:
+        x.append(lines[i][0])
+        y.append(lines[i][1])
+        z.append(lines[i][2])
+        d.append(lines[i][3])
 
-print("Values for ", xi, yi, zi, " are ", di[0])
+        i += 1
 
-# i = 0
+    rbfi = Rbf(x, y, z, d)  # radial basis function interpolator instance
+    # coordinates to be interpolated / predicted
 
-# while i < rbfi.N:
+    i = 0
+    xi = []
+    yi = []
+    zi = []
 
-#     print("Node {0} X: {1} Y: {2} Z: {3} Value: {4}".format(
-#         i, rbfi.xi[0][i], rbfi.xi[1][i], rbfi.xi[2][i], rbfi.di[i]))
+    while i < a:
+        xi = np.append(xi, np.linspace(-n+1, n-1, a))
+        yi = np.append(yi, np.linspace(0, 0, a))
+        zi = np.append(zi, np.linspace(-n+1, n-1, a))
 
-#     i += 1
+        i += 1
+
+    di = rbfi(xi, yi, zi)   # interpolated values
+
+    i = 0
+
+    with open("static/sensor_data.csv", "w", newline='') as f:
+
+        writer = csv.writer(f)
+
+        while i < a**2:
+            f.write("{},{},{}".format(xi[i], zi[i], di[i]))
+            f.write("\n")
+
+            i += 1
+
+    return di
